24b80664efcb250c11adb170d3b8c7329e439cae
tosiab <mtabednarz@gmail.com>
2024-02-26 14:36:33 -0500

    rect

---
+++ Rect.java
@@ -0,0 +1,18 @@
+/**
+ * project: Rectangle
+ * programmer: Tosia Bednarz
+ * date: February 26th, 2024
+ * program: Rect.java
+ ***/
+
+ public class Rect{
+     public static void main(String[]args){
+         System.out.println("***************");
+         System.out.println("*             *");
+         System.out.println("*             *");
+         System.out.println("*             *");
+         System.out.println("*             *");
+         System.out.println("*             *");
+         System.out.println("***************");
+     }
+ }
