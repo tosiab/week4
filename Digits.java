af37442952e8c128a28271a2ce8c9e35c10a2432
tosiab <mtabednarz@gmail.com>
2024-02-27 14:59:22 -0500

    digits
    finish at home

---
+++ Digits.java
@@ -0,0 +1,11 @@
+public class Digits {
+    public static void main(String[]args){
+      int number;
+      int num1;
+      int num2;
+      int num3;
+      number = 258;
+      num1 = (number%(number/100));
+    }
+    
+}
