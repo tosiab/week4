09b4c5883b08a581629e0b9c324548cff96007f3
tosiab <mtabednarz@gmail.com>
2024-02-28 14:26:46 -0500

    digits

--- Digits.java
+++ Digits.java
@@ -5,7 +5,13 @@ public class Digits {
       int num2;
       int num3;
       number = 258;
-      num1 = (number%(number/100));
+      num3 = (number%10);
+      num2 = (((number%100)-num3)/10);
+      num1 = (((number%1000)-(number%100))/100);
+
+      System.out.println("The hundreds digit is " + num1);
+      System.out.println("The tens digit is " + num2);
+      System.out.println("The ones digit is " + num3);
     }
     
 }
